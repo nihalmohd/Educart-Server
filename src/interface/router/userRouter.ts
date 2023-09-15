23eb98e43729818f1f7eeeb0fc629781fa1c9f83
nihalmohd <nihalmohd828@gmail.com>
2023-09-15 20:54:13 +0530

    checking

--- src/interface/router/userRouter.ts
+++ src/interface/router/userRouter.ts
@@ -26,39 +26,39 @@ userRouter.get("/",(req:Request,res:Response)=>{
     console.log("working");
     res.status(200).json({status:"done"})
 })
-//Post sign Up
-userRouter.post("/user/register",register)
-userRouter.post("/user/login",login)
-userRouter.post("/user/OTP",OTPAuth)
-userRouter.post("/user/ForgotPassword",ForgotOtp)
-userRouter.post("/user/VerifyOTP",OTPVerification)
-// Home get
-userRouter.get("/user/GetBanners",UserAutherization,AdminShowBanner)
-userRouter.get("/ShowCategory",UserAutherization,Showcategory)
-userRouter.get("/showCourse",UserAutherization,showCourse)
-userRouter.get('/TakeCouresByCategory',UserAutherization,TakeCourseByCategory)
+// //Post sign Up
+// userRouter.post("/user/register",register)
+// userRouter.post("/user/login",login)
+// userRouter.post("/user/OTP",OTPAuth)
+// userRouter.post("/user/ForgotPassword",ForgotOtp)
+// userRouter.post("/user/VerifyOTP",OTPVerification)
+// // Home get
+// userRouter.get("/user/GetBanners",UserAutherization,AdminShowBanner)
+// userRouter.get("/ShowCategory",UserAutherization,Showcategory)
+// userRouter.get("/showCourse",UserAutherization,showCourse)
+// userRouter.get('/TakeCouresByCategory',UserAutherization,TakeCourseByCategory)
 
 
-//profile get 
-userRouter.get('/ProfileTakeUser',UserAutherization,UserTakeProfile)
+// //profile get 
+// userRouter.get('/ProfileTakeUser',UserAutherization,UserTakeProfile)
 
-//profile post
-userRouter.post('/UpdateProfile',UserAutherization,UserProfileUpdate)
+// //profile post
+// userRouter.post('/UpdateProfile',UserAutherization,UserProfileUpdate)
 
-//Course Details get 
-userRouter.get("/CourseDeatailsByid",UserAutherization,showCourseByid)
+// //Course Details get 
+// userRouter.get("/CourseDeatailsByid",UserAutherization,showCourseByid)
 
-//Course Details post
-userRouter.post("/UpdateCouseid",UserAutherization,UerCouseListUpdate)
-userRouter.post("/UpdatedCouseIdtake",UserAutherization,UserTakeCourseIdOnUser)
+// //Course Details post
+// userRouter.post("/UpdateCouseid",UserAutherization,UerCouseListUpdate)
+// userRouter.post("/UpdatedCouseIdtake",UserAutherization,UserTakeCourseIdOnUser)
 
-//Category
-userRouter.get("/takeSubcategory",UserAutherization,UserTakeSubCategory)
-userRouter.get("/TakeMycourses",UserAutherization,takeMycourses)
+// //Category
+// userRouter.get("/takeSubcategory",UserAutherization,UserTakeSubCategory)
+// userRouter.get("/TakeMycourses",UserAutherization,takeMycourses)
 
-//Chat 
-userRouter.post ("/AddChat",UserAutherization,AddChat)
-userRouter.get("/GetMentors",UserAutherization,TakeMentors)
+// //Chat 
+// userRouter.post ("/AddChat",UserAutherization,AddChat)
+// userRouter.get("/GetMentors",UserAutherization,TakeMentors)
 
 
 
