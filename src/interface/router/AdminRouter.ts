986b355a93d8c85246319d2e5fcaa8d669d806f2
nihalmohd <nihalmohd828@gmail.com>
2023-09-15 21:30:24 +0530

    Mentor side and admin side always uncommented

--- src/interface/router/AdminRouter.ts
+++ src/interface/router/AdminRouter.ts
@@ -29,24 +29,24 @@ AdminRouter.get("/Admin", (req: Request, res: Response) => {
 
 // post methods
 
-// AdminRouter.post("/Login", Adminlogin);
-// AdminRouter.post("/getUsers", AdminAutherization,DisplayUsers)
-// AdminRouter.post("/blockUser",AdminAutherization, BlockUserReq)
-// AdminRouter.post("/UnblockUser",AdminAutherization, UnBlockUserReq)
-// AdminRouter.post("/getMentors",AdminAutherization, DisplayMentors)
-// AdminRouter.post("/blockMentor",AdminAutherization, BlockMentorReq)
-// AdminRouter.post("/UnblockMentor",AdminAutherization, UnBlockMentorReq)
-// AdminRouter.post("/BannerUpload",AdminAutherization, AdminCreateBanner)
-// AdminRouter.post("/AdminShowBanner",AdminAutherization,AdminShowBanner)
-// AdminRouter.post("/AdminHideBanner",AdminAutherization,AdminHideBanner) 
-// AdminRouter.post("/AdminVisibleBanner",AdminAutherization,AdminVisibleBanner) 
-// AdminRouter.post("/AdminCategory",AdminAutherization,Addcategory)
-// AdminRouter.post("/AdminDisplayCategory",AdminAutherization,ShowCategory)
-// AdminRouter.post("/AdminBlockCategory" ,AdminAutherization,AdminBlockCategory)
-// AdminRouter.post("/AdminUnBlockCategory" ,AdminAutherization,AdminUnBlockCategory)
-// AdminRouter.post("/AdminAddSubcategory",AdminAutherization,AdminAddSubcategory)
-// AdminRouter.get("/AdminDisplayCourse",AdminAutherization,AdminDisplayCourse)
-// AdminRouter.post("/AdminCourseVisible",AdminAutherization,AdminBlaockCourse)
-// AdminRouter.post("/AdminCourseInvisible",AdminAutherization,AdminUnblaockCourse)
+AdminRouter.post("/Login", Adminlogin);
+AdminRouter.post("/getUsers", AdminAutherization,DisplayUsers)
+AdminRouter.post("/blockUser",AdminAutherization, BlockUserReq)
+AdminRouter.post("/UnblockUser",AdminAutherization, UnBlockUserReq)
+AdminRouter.post("/getMentors",AdminAutherization, DisplayMentors)
+AdminRouter.post("/blockMentor",AdminAutherization, BlockMentorReq)
+AdminRouter.post("/UnblockMentor",AdminAutherization, UnBlockMentorReq)
+AdminRouter.post("/BannerUpload",AdminAutherization, AdminCreateBanner)
+AdminRouter.post("/AdminShowBanner",AdminAutherization,AdminShowBanner)
+AdminRouter.post("/AdminHideBanner",AdminAutherization,AdminHideBanner) 
+AdminRouter.post("/AdminVisibleBanner",AdminAutherization,AdminVisibleBanner) 
+AdminRouter.post("/AdminCategory",AdminAutherization,Addcategory)
+AdminRouter.post("/AdminDisplayCategory",AdminAutherization,ShowCategory)
+AdminRouter.post("/AdminBlockCategory" ,AdminAutherization,AdminBlockCategory)
+AdminRouter.post("/AdminUnBlockCategory" ,AdminAutherization,AdminUnBlockCategory)
+AdminRouter.post("/AdminAddSubcategory",AdminAutherization,AdminAddSubcategory)
+AdminRouter.get("/AdminDisplayCourse",AdminAutherization,AdminDisplayCourse)
+AdminRouter.post("/AdminCourseVisible",AdminAutherization,AdminBlaockCourse)
+AdminRouter.post("/AdminCourseInvisible",AdminAutherization,AdminUnblaockCourse)
 
 
